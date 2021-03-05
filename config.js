e9bf4908651bff0ee95e8cb092b13818e0ab6020
weiiyii <cwy.sophiechen@knights.ucf.edu>
2021-03-05 16:11:50 -0500

    .

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // mySMP is the name of the database, don't have to manually create it on atlas
+  // will create the db automatically if specified here
+  MONGODB:
+    "mongodb+srv://sophie:7USA2ZST8x6iJGbx@cluster0.or1pb.mongodb.net/mySMP?retryWrites=true&w=majority",
+  SECRET_KEY: "some very secret key",
+};
